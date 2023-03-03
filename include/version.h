eaa49b1327a50380e36be8f3667da73174e9c72c
nename0 <26363498+nename0@users.noreply.github.com>
2023-03-03 23:02:31 +0100

    Android: convert makefile to blueprint

---
+++ include/version.h
@@ -0,0 +1,7 @@
+#define SND_LIB_MAJOR 1
+#define SND_LIB_MINOR 2
+#define SND_LIB_SUBMINOR 8
+#define SND_LIB_EXTRAVER 1000000
+#define SND_LIB_VER(maj, min, sub) (((maj)<<16)|((min)<<8)|(sub))
+#define SND_LIB_VERSION SND_LIB_VER(SND_LIB_MAJOR, SND_LIB_MINOR, SND_LIB_SUBMINOR)
+#define SND_LIB_VERSION_STR "1.2.8"
